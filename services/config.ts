a5a4a5305cdec199f09ba7789ade2e0ee520cbf0
ankitahota32 <ankitahota3264@gmail.com>
2025-06-27 13:28:10 +0530

    minor changes

--- services/config.ts
+++ services/config.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const Api = axios.create({
-  baseURL: "https://gsap-landing-backend.onrender.com",
+  baseURL: "https://gsap-landing-backend.onrender.com/api",
 });
