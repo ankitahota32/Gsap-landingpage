97a1c5775433cb00a99463b177781ef94fbe3cdd
ankitahota32 <ankitahota3264@gmail.com>
2025-07-04 13:19:51 +0530

    error fixed

--- services/config.ts
+++ services/config.ts
@@ -3,8 +3,6 @@ import axios from "axios";
 const isProd = process.env.NODE_ENV === "production"
 
 export const Api = axios.create({
-  baseURL: isProd 
-    ? "https://gsap-landing-backend.onrender.com/api"
-    : "http://localhost:4000/api",
+  baseURL: "https://gsap-landing-backend.onrender.com/api"
   withCredentials: true,
 });
